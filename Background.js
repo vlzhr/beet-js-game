b2d8d1e2ea0f79cfd8950c1be2a3d40682315c06
Vladimir Zhuravlev <izhur27@gmail.com>
2019-02-09 00:28:48 +0100

    background parralax added

---
+++ Background.js
@@ -0,0 +1,50 @@
+class BackgroundPart extends PIXI.Sprite {
+    constructor(arg) {
+        super(resources[arg].texture);
+
+        this.initProportion = this.width / this.height;
+        this.height = app.renderer.height;
+        this.width = this.height * this.initProportion;
+
+        app.stage.addChild(this);
+    }
+}
+
+class StaticBackground extends PIXI.Sprite {
+    constructor(arg) {
+        super(resources[arg].texture);
+
+        this.anchor.set(0.5-(0.5*app.renderer.width/this.width), 0);
+
+        this.initProportion = this.width / this.height;
+        this.height = app.renderer.height;
+        this.width = this.height * this.initProportion;
+
+        app.stage.addChild(this);
+    }
+}
+
+class Background {
+    constructor(img="images/background/1.png", speed=0) {
+
+        this.parts = [
+            new BackgroundPart(img),
+            new BackgroundPart(img)
+        ];
+
+        // this.parts[1].scale.x = -1;
+
+        this.img = img;
+        this.speed = speed;
+    }
+
+    nextStep(xSpeedFactor) {
+        for (let partN in this.parts) {
+            const part = this.parts[partN];
+            part.x -= this.speed*xSpeedFactor;
+            if (part.x < -part.width) {
+                part.x = part.width;
+            }
+        }
+    }
+}
--- Enemy.js
+++ Enemy.js
@@ -12,19 +12,36 @@ class Enemy extends Sprite {
 }
 
 
-class Barrier extends Sprite {
-    constructor() {
+class BarrierElement extends Sprite {
+    constructor(y) {
         super(resources["images/meat-grinder.png"].texture);
 
         this.width = 80;
         this.height = 80;
 
-        this.y = app.renderer.height-150;
-        this.x = app.renderer.width-this.width-10;
+        this.y = y;
+        this.x = app.renderer.width+this.width-10;
 
-        this.speed = 2;
+        this.speed = 2.5;
+        this.anchor.set(0.5);
 
         app.stage.addChild(this);
+    }
+}
+
+class Barrier {
+    constructor() {
+        this.elements = [];
+        for (let n=0; n < Math.floor(Math.random()*4)+2; n++) {
+            this.elements.push(new BarrierElement(app.renderer.height-80-n*80));
+        }
+    }
+
+    move(xPlusFactor) {
+        for (let n in this.elements) {
+            this.elements[n].x -= this.elements[n].speed * (xPlusFactor ? xPlusFactor : 1);
+            this.elements[n].rotation += 0.1;
+        }
     }
 }
 
